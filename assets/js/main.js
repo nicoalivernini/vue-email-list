7351f085043d02ec95c3fb3d1e9b1518f518da0a
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-08 16:39:26 +0200

    Aggiornamento

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,21 @@ var app = new Vue ({
 
   el: '#root',
   data: {
-    mails: 10,
-    indirizzo: [],
+    indirizzoMail: [],
 
   },//Chiusura data
   methods: {
-    indirizzoEmail: function () {
-      for (var i = 0; i <= 10; i++) {
+    newMail: function () {
+      for (var i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) => {
-          this.indirizzo.push(response.data.response)
+          this.indirizzoMail.push(response.data.response)
         });
       }
-
-      //console.log(this.indirizzo);
     },
 
     clearMail: function () {
-      this.indirizzo = [];
+      this.indirizzoMail = [];
     }
 
 
