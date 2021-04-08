649deec946eac1465f4900622bad4667c718f986
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-08 15:52:51 +0200

    Email generate

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,21 @@ var app = new Vue ({
 
   el: '#root',
   data: {
+    mails: 10,
+    indirizzo: [],
 
   },//Chiusura data
   methods: {
+    indirizzoEmail: function () {
+      for (var i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+          this.indirizzo.push(response.data.response)
+        });
+      }
+      //console.log(this.indirizzo);
+      }
+
 
   }//Chiusura Methods
 })//Chiusura Vue
