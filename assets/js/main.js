9d87dc043de129d021c8df1ee8b051d3057ae637
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-08 15:17:01 +0200

    Caricamento file di base

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Genera 10 mail tramite API e stampale in una lista
+
+var app = new Vue ({
+
+  el: '#root',
+  data: {
+
+  },//Chiusura data
+  methods: {
+
+  }//Chiusura Methods
+})//Chiusura Vue
