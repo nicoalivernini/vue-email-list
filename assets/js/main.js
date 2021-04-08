8d68410bb33ce98ea559d1147109b5ceed67c5ea
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-08 15:58:00 +0200

    Aggiunta pulsante pulizia mail

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,18 @@ var app = new Vue ({
   },//Chiusura data
   methods: {
     indirizzoEmail: function () {
-      for (var i = 0; i < 10; i++) {
+      for (var i = 0; i <= 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then((response) => {
           this.indirizzo.push(response.data.response)
         });
       }
       //console.log(this.indirizzo);
-      }
+    },
+
+    clearMail: function () {
+      this.indirizzo = [];
+    }
 
 
   }//Chiusura Methods
